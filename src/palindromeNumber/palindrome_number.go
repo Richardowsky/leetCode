cbd337a6c12e82ac5768c4872bdaa5ce3b088224
richardowsky <brooklyn.od.ua@gmail.com>
2020-11-06 00:07:24 +0200

    solution palindrome number

--- src/palindromeNumber/palindrome_number.go
+++ src/palindromeNumber/palindrome_number.go
@@ -1,1 +1,26 @@
 package palindromeNumber
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	n := x
+	big := 10
+	small := 1
+	for n != 0 {
+		n = n / 10
+		back := x % big / small
+		front := x
+		for front > big-1 {
+			front = front / 10
+		}
+		front = front % 10
+		if front != back {
+			return false
+		}
+		big = big * 10
+		small = small * 10
+	}
+
+	return true
+}
--- src/palindromeNumber/palindrome_number_test.go
+++ src/palindromeNumber/palindrome_number_test.go
@@ -1,1 +1,23 @@
 package palindromeNumber
+
+import (
+	"github.com/magiconair/properties/assert"
+	"testing"
+)
+
+var str = 10101
+
+func TestList(t *testing.T) {
+	excpect := true
+	assert.Equal(t, isPalindrome(str), excpect)
+}
+
+func Benchmark(b *testing.B) {
+	benchmarkZero(str, b)
+}
+
+func benchmarkZero(str int, b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		isPalindrome(str)
+	}
+}
